f4b1827f2ac4b67debb3d8b4556fc2fad4ebee81
Robert Papazyan <robypapazyan@gmail.com>
2025-10-21 22:41:49 +0300

    Add Firebird fixture data for MATERIAL and BARCODE (SQL + Excel test sets)

---
+++ tests/fixtures/barcodematerial.sql
@@ -0,0 +1,42 @@
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 872, 'K008802A_V01');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 1045, 'CD-R(ЗАПИСВАЩО CD) 650 MB');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 328, 'SD KARTA 16 GB МИКРО cl10 tray');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 214, 'MONITOR 19 LG 19MB15T-I /TOUCH SCREEN');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 323, 'SUPREMO- 1 допълнителна конекция');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 428, 'КАБЕЛ 803 ВРЪЗКА PC-EVL/EP');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 1046, 'ХАРТИЕН ПЛИК ЗА CD 650 MB');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 958, 'ТЕРМОТРАНСФЕРЕН ПРИНТЕР АЛФА');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 300, 'МИСТРАЛ V3 Моб.склад-годишен лиценз');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 38, 'МИСТРАЛ V3 - Back Office - годишен абонамент');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 782, 'МИСТРАЛ 3 - Back Office - договор за 6 месеца');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 37, 'МИСТРАЛ V3 - Front Office - годишен абонамент');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 783, 'МИСТРАЛ 3 - Front Office - договор за 6 месеца');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 1074, 'ФИСКАЛНО УСТРОЙСТВО BLUECASH-500');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 853, 'ПОС принтер Datecs EP-700');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 2, 'ТРУД');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 125, 'Захранване 450 W');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 154, 'Замбу-Back Office годишен абонамет');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 153, 'Замбу -Front Office годишен абонамет');
+INSERT INTO BARCODE (CODE, MATERIALCODE, MATERIAL)
+             VALUES ('', 327, 'Кабел FM 12 см 10 пин');
+
+COMMIT WORK;
